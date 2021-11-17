7da6dafd6b42436a067570b0d1675085df249dac
Michal Srb <xixixao@seznam.cz>
2021-11-17 10:52:06 -0800

    Initial integration with react flow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,122 @@ import { useImmer } from "use-immer";
 import { produce, original } from "immer";
 import initSqlJs from "sql.js";
 
+import ReactFlow, {
+  removeElements,
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+} from "react-flow-renderer";
+
+const initialElements = [
+  {
+    id: "1",
+    type: "input",
+    data: {
+      label: (
+        <>
+          Welcome to <strong>React Flow!</strong>
+        </>
+      ),
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: "2",
+    data: {
+      label: (
+        <>
+          This is a <strong>default node</strong>
+        </>
+      ),
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: "3",
+    data: {
+      label: (
+        <>
+          This one has a <strong>custom style</strong>
+        </>
+      ),
+    },
+    position: { x: 400, y: 100 },
+    style: {
+      background: "#D6D5E6",
+      color: "#333",
+      border: "1px solid #222138",
+      width: 180,
+    },
+  },
+  {
+    id: "4",
+    position: { x: 250, y: 200 },
+    data: {
+      label: "Another default node",
+    },
+  },
+  {
+    id: "5",
+    data: {
+      label: "Node id: 5",
+    },
+    position: { x: 250, y: 325 },
+  },
+  {
+    id: "6",
+    type: "output",
+    data: {
+      label: (
+        <>
+          An <strong>output node</strong>
+        </>
+      ),
+    },
+    position: { x: 100, y: 480 },
+  },
+  {
+    id: "7",
+    type: "output",
+    data: { label: "Another output node" },
+    position: { x: 400, y: 450 },
+  },
+  { id: "e1-2", source: "1", target: "2", label: "this is an edge label" },
+  { id: "e1-3", source: "1", target: "3" },
+  {
+    id: "e3-4",
+    source: "3",
+    target: "4",
+    animated: true,
+    label: "animated edge",
+  },
+  {
+    id: "e4-5",
+    source: "4",
+    target: "5",
+    arrowHeadType: "arrowclosed",
+    label: "edge with arrow head",
+  },
+  {
+    id: "e5-6",
+    source: "5",
+    target: "6",
+    type: "smoothstep",
+    label: "smooth step edge",
+  },
+  {
+    id: "e5-7",
+    source: "5",
+    target: "7",
+    type: "step",
+    style: { stroke: "#f6ab6c" },
+    label: "a step edge",
+    animated: true,
+    labelStyle: { fill: "#f6ab6c", fontWeight: 700 },
+  },
+];
+
 const database = initSqlJs({
   // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
   // You can omit locateFile completely when running in node
@@ -56,6 +172,15 @@ function App() {
       >
         <div
           style={{
+            height: "65%",
+            borderBottom: "1px solid #ccc",
+            borderTop: "1px solid #ccc",
+          }}
+        >
+          <NodesPane nodeState={nodeState} setNodeState={setNodeState} />
+        </div>
+        {/* <div
+          style={{
             backgroundImage:
               "radial-gradient(circle at 1px 1px, #e8e8e8 1px, transparent 0)",
             backgroundSize: "16px 16px",
@@ -87,12 +212,62 @@ function App() {
               +FROM
             </AddNodeButton>
           </div>
-        </div>
+        </div> */}
         <div style={{ padding: 8, overflowX: "scroll", flexGrow: 1 }}>
           <Table nodeState={nodeState} setNodeState={setNodeState} />
         </div>
       </div>
     </>
+  );
+}
+
+// const onLoad = (reactFlowInstance) => {
+//   console.log("flow loaded:", reactFlowInstance);
+//   // reactFlowInstance.fitView();
+// };
+
+function NodesPane({ nodeState, setNodeState }) {
+  const [elements, setElements] = useState(initialElements);
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+
+  return (
+    <ReactFlow
+      elements={Object.values(nodeState.nodes).map((node) => {
+        return {
+          id: node.id,
+          type: "from",
+          data: { node, nodeState, setNodeState },
+          position: { x: 40, y: 10 },
+        };
+      })}
+      nodeTypes={{
+        from: FromNode.Component,
+      }}
+      onElementsRemove={onElementsRemove}
+      onConnect={onConnect}
+      // onLoad={onLoad}
+    >
+      {/* <MiniMap
+        nodeStrokeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+          if (n.type === "input") return "#0041d0";
+          if (n.type === "output") return "#ff0072";
+          if (n.type === "default") return "#1a192b";
+
+          return "#eee";
+        }}
+        nodeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+
+          return "#fff";
+        }}
+        nodeBorderRadius={2}
+      /> */}
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
   );
 }
 
@@ -128,7 +303,7 @@ const DeletedNode = {
 
 const FromNode = {
   name: "FromNode",
-  Component({ node, nodeState, setNodeState, setTable }) {
+  Component({ data: { node, nodeState, setNodeState } }) {
     const { id, name } = node;
     return (
       <NodeUI
@@ -166,7 +341,7 @@ const FromNode = {
 
 const SelectNode = {
   name: "SelectNode",
-  Component({ node, nodeState, setNodeState, setTable }) {
+  Component({ node, nodeState, setNodeState }) {
     // const selectedColumnNames = SelectNode.columnNames(nodeState, node);
     return (
       <NodeUI
@@ -280,7 +455,7 @@ const SelectNode = {
 
 const WhereNode = {
   name: "WhereNode",
-  Component({ node, nodeState, setNodeState, setTable }) {
+  Component({ node, nodeState, setNodeState }) {
     // const { filters: _ } = node;
     return (
       <NodeUI
@@ -308,7 +483,7 @@ const WhereNode = {
   },
 };
 
-function GroupNodeComponent({ node, nodeState, setNodeState, setTable }) {
+function GroupNodeComponent({ node, nodeState, setNodeState }) {
   const selectedColumnNames = SelectNode.columnNames(nodeState, node);
   return (
     <NodeUI
@@ -396,7 +571,7 @@ const GroupNode = {
 
 const OrderNode = {
   name: "OrderNode",
-  Component({ node, nodeState, setNodeState, setTable }) {
+  Component({ node, nodeState, setNodeState }) {
     return (
       <NodeInput
         label="ORDER BY"
