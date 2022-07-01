1bf5e674cceb131ba3184d2a33072b6e084b0a1b
Michal Srb <xixixao@seznam.cz>
2022-07-01 19:58:13 -0400

    Again reworked results display, this time to flex the two columns to max 50%, working well

--- src/sql/results/SQLResults.jsx
+++ src/sql/results/SQLResults.jsx
@@ -1,9 +1,12 @@
 import * as Nodes from "graph/Nodes";
 import { only } from "js/Arrays";
 import React, { createContext, Fragment, useContext } from "react";
+import { Box } from "ui/layout/Box";
 import HorizontalSpace from "ui/layout/HorizontalSpace";
 import { Row } from "ui/layout/Row";
 import { useGetNodeConfig } from "../sqlNodes";
+import { Children } from "react";
+import { invariant } from "js/invariant";
 
 const Context = createContext();
 export function useSQLResultsNodeContext() {
@@ -18,7 +21,6 @@ export function useIsThisOnlySelectedNode() {
 export function SQLResults({ appState }) {
   const selected = Nodes.selected(appState);
   const onlyLastSelected = only(Nodes.lastSelected(appState));
-  const getConfig = useGetNodeConfig();
   if (selected.length > 2) {
     return null;
   }
@@ -26,6 +28,7 @@ export function SQLResults({ appState }) {
     selected.length === 0 && onlyLastSelected != null
       ? [onlyLastSelected]
       : selected;
+  const [first, second] = shown;
   return (
     <Row
       css={{
@@ -34,22 +37,61 @@ export function SQLResults({ appState }) {
         maxHeight: "100%",
       }}
     >
-      {shown.map((node, i) => {
-        const Results =
-          getConfig(node).Results ??
-          (() => {
-            return null;
-          });
-        return (
-          <Fragment key={i}>
-            <Context.Provider value={{ appState, node }}>
-              <Results />
-            </Context.Provider>
-            <HorizontalSpace />
-            <HorizontalSpace />
-          </Fragment>
-        );
-      })}
+      {first == null ? null : second == null ? (
+        <Result appState={appState} node={first} />
+      ) : (
+        <HorizontalSplitView>
+          <Result appState={appState} node={first} />
+          <Result appState={appState} node={second} />
+        </HorizontalSplitView>
+      )}
     </Row>
+  );
+}
+
+function Result({ appState, node }) {
+  const getConfig = useGetNodeConfig();
+  const Results =
+    getConfig(node).Results ??
+    (() => {
+      return null;
+    });
+  return (
+    <Context.Provider value={{ appState, node }}>
+      <Results />
+    </Context.Provider>
+  );
+}
+
+function HorizontalSplitView({ children }) {
+  const childrenArray = Children.toArray(children);
+  invariant(childrenArray.length === 2, "Split view can have only 2 children");
+  const [first, second] = childrenArray;
+  return (
+    <>
+      <Box
+        css={{
+          height: "fit-content",
+          flex: "1 0 0",
+          overflowX: "scroll",
+          maxWidth: "fit-content",
+          borderRight: "1px solid $slate7",
+        }}
+      >
+        {first}
+      </Box>
+      <HorizontalSpace />
+      <HorizontalSpace />
+      <Box
+        css={{
+          height: "fit-content",
+          flex: "1 0 0",
+          overflowX: "scroll",
+          maxWidth: "fit-content",
+        }}
+      >
+        {second}
+      </Box>
+    </>
   );
 }
